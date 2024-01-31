fb0f9dee7519c0bcca8a4f26fb6fc3e15134ccb4
yeonwlee <tlstprl1806@naver.com>
2024-01-31 13:43:22 +0900

    [Feat]Number of lines to write string

--- leetcode/806. number-of-lines-to-write-string.py
+++ leetcode/806. number-of-lines-to-write-string.py
@@ -1,29 +1,29 @@
 import string
 
-# def number_of_lines(widths, sentence):
-#     #dict를 만들면 좋을 것 같다.
-#     count_of_lines = 1
-#     width_info = _make_width_info(widths, string.ascii_lowercase)
-#     #그리고 정렬. 앞에서부터 수를 꺼내면, 배열 요소를 움직여줘야하니 뒤에서 뽑자.
-#     sorted_sentence_by_width = sorted(list(sentence), key=lambda x: width_info[x], reverse=True)
-#     cur_line_width = 0
-#     while sorted_sentence_by_width:
-#         cur_char = sorted_sentence_by_width[-1]
-#         if cur_line_width + width_info[cur_char] <= 100:
-#             cur_line_width += width_info[cur_char]
-#             sorted_sentence_by_width.pop()
-#         else:
-#             count_of_lines += 1
-#             cur_line_width = 0
-#     return [count_of_lines, cur_line_width] 
-# def _make_width_info(widths, charset_info):
-#     width_info = {}
-#     for index, character in enumerate(charset_info):
-#         width_info[character] = widths[index]
-#     return width_info
+
+###실수1 문제를 잘못 이해함.
+###+굳이 내림차순으로 정렬할 필요도, while로 돌면서 pop할 필요 없었다. 이해한 바 대로 풀어도 그냥 오름차순 정렬에 순회로 했어도 됐다.
+def number_of_lines(widths, sentence):
+    count_of_lines = 1
+    width_info = _make_width_info(widths, string.ascii_lowercase)
+    sorted_sentence_by_width = sorted(list(sentence), key=lambda x: width_info[x], reverse=True)
+    cur_line_width = 0
+    while sorted_sentence_by_width: 
+        cur_char = sorted_sentence_by_width[-1]
+        if cur_line_width + width_info[cur_char] <= 100:
+            cur_line_width += width_info[cur_char]
+        else:
+            count_of_lines += 1
+            cur_line_width = width_info[cur_char]
+        sorted_sentence_by_width.pop()    
+    return [count_of_lines, cur_line_width]
+def _make_width_info(widths, charset_info):
+    width_info = {}
+    for index, character in enumerate(charset_info):
+        width_info[character] = widths[index]
+    return width_info
 
 
-###실수1 문제를 잘못 이해함. 주어진 문자의 순서는 그대로여야했다.
 class Solution:
     def _make_width_info(self, widths, charset_info):
         width_info = {}
