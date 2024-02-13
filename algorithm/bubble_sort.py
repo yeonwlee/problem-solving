26b3a440f39d6e31ea8d9fa2d199c883d2f13536
yeonwlee <tlstprl1806@naver.com>
2024-02-13 12:57:14 +0900

    [Chore]누락된 타입 힌팅 추가

--- algorithm/bubble_sort.py
+++ algorithm/bubble_sort.py
@@ -1,15 +1,17 @@
 #현재 target은 시퀀스형을 지원하지 않고 있음. 리스트 한정
 #정렬 기준은 오름차순. 현재 파라미터 reverse는 쓰이고 있지 않음.
 class BubbleSort:
-	def sort(self, target: list, reverse: bool = False) -> list: 
+	def sort(self, target: list, reverse: bool = False) -> list:
+		is_all_paramters_valid: bool
+		message: str
 		is_all_paramters_valid, message = self._are_valid_parameters(target, reverse)
 		if not is_all_paramters_valid:
 			raise ValueError(message)
-		num_of_sorted_value = 0
+		num_of_sorted_value: int = 0
 		while num_of_sorted_value != len(target):
-			cur_most_bigger_index = 0
+			cur_most_bigger_index: int = 0
 			while cur_most_bigger_index != len(target) - 1 - num_of_sorted_value:
-				next_index = cur_most_bigger_index + 1
+				next_index: int = cur_most_bigger_index + 1
 				if self._is_bigger_than_next_value(target[cur_most_bigger_index], target[next_index]):
 					self._exchange_position(target, cur_most_bigger_index, next_index)
 				cur_most_bigger_index += 1
@@ -17,7 +19,7 @@ class BubbleSort:
 		return target
 
 
-	def _are_valid_parameters(self, target, reverse):
+	def _are_valid_parameters(self, target: list, reverse: bool) -> tuple[bool, str]:
 		if not isinstance(target, list):
 			return (False, "first parameter must be a list")
 		if not isinstance(reverse, bool):
@@ -29,7 +31,7 @@ class BubbleSort:
 		return cur_value > next_value
 
 
-	def _exchange_position(self, target: list, cur_index: int, next_index: int):
+	def _exchange_position(self, target: list, cur_index: int, next_index: int) -> None:
 		target[cur_index], target[next_index] = target[next_index], target[cur_index]
 
 
